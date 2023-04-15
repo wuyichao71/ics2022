e2132f4271fd0c0f621e263f6ef8f10c0172d657
tracer-ics2022 <tracer@njuics.org>
2023-04-15 15:24:56 +0800

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux linux-26ae 5.14.21-150400.24.46-default #1 SMP PREEMPT_DYNAMIC Thu Feb 9 08:38:18 UTC 2023 (2d95137) x86_64 x86_64 x86_64 GNU/Linux  15:24:56  已启动 7 天13:43，4 个用户，平均负载：0.18, 0.25, 0.17

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -15,14 +15,13 @@
 
 #include <utils.h>
 
-/* NEMUState nemu_state = { .state = NEMU_STOP }; */
-NEMUState nemu_state = { .state = NEMU_END };
+NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
-  int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
-    (nemu_state.state == NEMU_QUIT);
-  /* int good = (nemu_state.state == NEMU_STOP && nemu_state.halt_ret == 0) || */
+  /* int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) || */
   /*   (nemu_state.state == NEMU_QUIT); */
+  int good = (nemu_state.state == NEMU_STOP && nemu_state.halt_ret == 0) ||
+    (nemu_state.state == NEMU_QUIT);
   return !good;
   /* return 1; */
 }
