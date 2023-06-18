a3994329caebbe478bbd152f1c8bf2491e235095
tracer-ics2022 <tracer@njuics.org>
2023-06-19 02:13:26 +0800

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux linux-26ae 5.14.21-150400.24.63-default #1 SMP PREEMPT_DYNAMIC Tue May 2 15:49:04 UTC 2023 (fd0cc4f) x86_64 x86_64 x86_64 GNU/Linux  02:13:26  已启动 4 天15:42，3 个用户，平均负载：0.16, 0.10, 0.08

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -60,7 +60,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len);
   p += space_len;
-  printf(FMT_WORD", "FMT_WORD, s->pc, s->snpc);
+  printf(FMT_WORD", "FMT_WORD"\n", s->pc, s->snpc);
 
 #ifndef CONFIG_ISA_loongarch32r
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
