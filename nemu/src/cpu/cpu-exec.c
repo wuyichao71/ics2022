471f0d13cc4346d87f312a70314d7777a0dc886a
tracer-ics2022 <tracer@njuics.org>
2023-06-19 02:16:24 +0800

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux linux-26ae 5.14.21-150400.24.63-default #1 SMP PREEMPT_DYNAMIC Tue May 2 15:49:04 UTC 2023 (fd0cc4f) x86_64 x86_64 x86_64 GNU/Linux  02:16:24  已启动 4 天15:44，3 个用户，平均负载：0.17, 0.12, 0.09

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -43,9 +43,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
   s->snpc = pc;
+  printf(FMT_WORD", "FMT_WORD", "FMT_WORD"\n", cpu.pc, s->pc, s->snpc);
   isa_exec_once(s);
   cpu.pc = s->dnpc;
-  printf(FMT_WORD", "FMT_WORD", "FMT_WORD"\n", cpu.pc, s->pc, s->snpc);
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
