311d0a156112bc8b7fd64e8519d6b4bc8c401f1e
tracer-ics2022 <tracer@njuics.org>
2023-06-19 01:57:37 +0800

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux linux-26ae 5.14.21-150400.24.63-default #1 SMP PREEMPT_DYNAMIC Tue May 2 15:49:04 UTC 2023 (fd0cc4f) x86_64 x86_64 x86_64 GNU/Linux  01:57:37  已启动 4 天15:26，3 个用户，平均负载：0.11, 0.07, 0.05

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -55,6 +55,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
     p += snprintf(p, 4, " %02x", inst[i]);
   }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
+  printf("%d\n", ilen_max);
   int space_len = ilen_max - ilen;
   if (space_len < 0) space_len = 0;
   space_len = space_len * 3 + 1;
