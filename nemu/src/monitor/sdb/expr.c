fda39abf4298c27008d6a8f2951cb42d0c4fcebd
tracer-ics2022 <tracer@njuics.org>
2023-06-22 01:41:31 +0800

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux linux-26ae 5.14.21-150400.24.63-default #1 SMP PREEMPT_DYNAMIC Tue May 2 15:49:04 UTC 2023 (fd0cc4f) x86_64 x86_64 x86_64 GNU/Linux  01:41:31  已启动 7 天15:10，3 个用户，平均负载：0.32, 0.17, 0.12

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -218,7 +218,7 @@ static int dominant_operator(int p, int q)
       level--;
     else if (level == 0)
     {
-      if (tokens[i].type != TK_NUM && tokens[i].type != TK_NEG)
+      if (tokens[i].type != TK_NUM)
       {
         cur_pri = priority(i);
         /* printf("%d\n", cur_pri); */
