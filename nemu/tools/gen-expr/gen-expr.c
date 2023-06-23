df9f80dbe995698f52f15b8ccb569847bb0962b3
tracer-ics2022 <tracer@njuics.org>
2023-06-24 00:28:59 +0800

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux linux-26ae 5.14.21-150400.24.63-default #1 SMP PREEMPT_DYNAMIC Tue May 2 15:49:04 UTC 2023 (fd0cc4f) x86_64 x86_64 x86_64 GNU/Linux  00:28:59  已启动 9 天13:57，3 个用户，平均负载：0.91, 0.54, 0.21

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -19,10 +19,14 @@
 #include <time.h>
 #include <assert.h>
 #include <string.h>
+/* wuyc */
+/* #include <errno.h> */
+#define BUFLEN 65536
+/* wuyc */
 
 // this should be enough
-static char buf[65536] = {};
-static char code_buf[65536 + 128] = {}; // a little larger than `buf`
+static char buf[BUFLEN] = {};
+static char code_buf[BUFLEN + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
@@ -31,8 +35,65 @@ static char *code_format =
 "  return 0; "
 "}";
 
+/* wuyc */
+#define ARRLEN(a) (sizeof(a) / sizeof(a[0]))
+#define GEN_VALUE(format, value) \
+  do { \
+    int str_len = 0; \
+    int rest_len = BUFLEN - buf_len; \
+    str_len = snprintf(buf+buf_len, BUFLEN - buf_len, format, value); \
+    if (str_len >= rest_len) { printf("buf = %s\n", buf); } \
+    assert(str_len < rest_len); \
+    buf_len += str_len; \
+  } while(0) 
+
+#define PROB 0.1
+
+static int buf_len = 0;
+static char *op_str[] = {"+", "-", "*", "/"};
+
+static void gen_space();
+
+static int choose(int i) {return rand() % i;}
+static void gen_num()
+{
+  /* int str_len = 0; */
+  /* str_len = snprintf(buf+buf_len, BUFLEN - buf_len, "%d", choose(65536)); */
+  /* printf("buf_len = %d\n", buf_len); */
+  GEN_VALUE("%d", choose(65536));
+  gen_space();
+}
+void gen(char c)
+{
+  /* buf_len += sprintf(buf+buf_len, "%c", c); */
+  GEN_VALUE("%c", c);
+}
+void gen_rand_op()
+{
+  int i = choose(ARRLEN(op_str));
+  GEN_VALUE("%s", op_str[i]);
+  gen_space();
+  /* buf_len += sprintf(buf+buf_len, "%s", op_str[i]); */
+}
+static void gen_space()
+{
+  float p = rand() / (float)RAND_MAX;
+  while(p < PROB) 
+  {
+    p = rand() / (float)RAND_MAX;
+    GEN_VALUE("%c", ' ');
+  }
+}
+/* wuyc */
+
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  /* gen_space(); */
+  switch(choose(3))
+  {
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -44,7 +105,9 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    buf_len = 0;
     gen_rand_expr();
+    /* printf("%d\n", choose(3)); */
 
     sprintf(code_buf, code_format, buf);
 
@@ -53,7 +116,8 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    int ret = system("gcc /tmp/.code.c -Wno-overflow -Werror -Wall -o /tmp/.expr");
+    printf("ret = %d\n", ret);
     if (ret != 0) continue;
 
     fp = popen("/tmp/.expr", "r");
