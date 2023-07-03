d51978dd666e85512afee191f8bbadb110e475d1
tracer-ics2022 <tracer@njuics.org>
2023-07-03 21:54:06 +0800

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux linux-26ae 5.14.21-150400.24.63-default #1 SMP PREEMPT_DYNAMIC Tue May 2 15:49:04 UTC 2023 (fd0cc4f) x86_64 x86_64 x86_64 GNU/Linux  21:54:06  已启动  2:10，4 个用户，平均负载：0.04, 0.16, 0.20

--- nemu/include/cpu/difftest.h
+++ nemu/include/cpu/difftest.h
@@ -24,6 +24,9 @@ void difftest_skip_ref();
 void difftest_skip_dut(int nr_ref, int nr_dut);
 void difftest_set_patch(void (*fn)(void *arg), void *arg);
 void difftest_step(vaddr_t pc, vaddr_t npc);
+/* wuyc */
+void difftest_watchpoint() {}
+/* wuyc */
 void difftest_detach();
 void difftest_attach();
 #else
@@ -31,6 +34,9 @@ static inline void difftest_skip_ref() {}
 static inline void difftest_skip_dut(int nr_ref, int nr_dut) {}
 static inline void difftest_set_patch(void (*fn)(void *arg), void *arg) {}
 static inline void difftest_step(vaddr_t pc, vaddr_t npc) {}
+/* wuyc */
+static inline void difftest_watchpoint() {}
+/* wuyc */
 static inline void difftest_detach() {}
 static inline void difftest_attach() {}
 #endif
--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -20,6 +20,9 @@
 #include <memory/paddr.h>
 #include <utils.h>
 #include <difftest-def.h>
+/* wuyc */
+#include <watchpoint.h>
+/* wuyc */
 
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
