c3abe6078207a245b041d2b9a9178f0f1bf6dfb1
tracer-ics2022 <tracer@njuics.org>
2023-07-03 21:57:08 +0800

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux linux-26ae 5.14.21-150400.24.63-default #1 SMP PREEMPT_DYNAMIC Tue May 2 15:49:04 UTC 2023 (fd0cc4f) x86_64 x86_64 x86_64 GNU/Linux  21:57:08  已启动  2:13，4 个用户，平均负载：0.17, 0.23, 0.22

--- nemu/include/cpu/difftest.h
+++ nemu/include/cpu/difftest.h
@@ -24,9 +24,7 @@ void difftest_skip_ref();
 void difftest_skip_dut(int nr_ref, int nr_dut);
 void difftest_set_patch(void (*fn)(void *arg), void *arg);
 void difftest_step(vaddr_t pc, vaddr_t npc);
-/* wuyc */
-void difftest_watchpoint() {}
-/* wuyc */
+void difftest_watchpoint();
 void difftest_detach();
 void difftest_attach();
 #else
