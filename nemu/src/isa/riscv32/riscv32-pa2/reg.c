65be54da754f8196aef4a02c61b4df346268293c
tracer-ics2022 <tracer@njuics.org>
2023-08-20 22:37:35 +0800

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux linux-26ae 5.14.21-150500.53-default #1 SMP PREEMPT_DYNAMIC Wed May 10 07:56:26 UTC 2023 (b630043) x86_64 x86_64 x86_64 GNU/Linux  22:37:35  已启动 11:25，4 个用户，平均负载：0.06, 0.11, 0.04

---
+++ nemu/src/isa/riscv32/riscv32-pa2/reg.c
@@ -0,0 +1,52 @@
+/***************************************************************************************
+* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#include <isa.h>
+#include "local-include/reg.h"
+
+const char *regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+void isa_reg_display() {
+  /* wuyc */
+  /* int regs_len = sizeof(regs) / sizeof(char *); */
+  int regs_len = ARRLEN(regs);
+  for(int i = 0; i < regs_len; i++)
+  {
+    printf("%4s: " FMT_WORD, reg_name(i, 4), gpr(i));
+    if ((i + 1) % 4 == 0)
+      printf("\n");
+  }
+  /* wuyc */
+}
+
+word_t isa_reg_str2val(const char *s, bool *success) {
+  int regs_len = ARRLEN(regs);
+  for(int i = 0; i < regs_len; i++)
+  {
+    if (strcmp(s, regs[i]) == 0)
+    {
+      *success = true;
+      return gpr(i);
+    }
+  }
+
+  *success = false;
+  return 0;
+}
