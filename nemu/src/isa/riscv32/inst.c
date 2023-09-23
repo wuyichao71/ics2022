db988a9c6d9df3e9f3b26f1d40146914837c4232
tracer-ics2022 <tracer@njuics.org>
2023-09-23 13:12:16 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.19-default #1 SMP PREEMPT_DYNAMIC Tue Aug 8 22:15:01 UTC 2023 (9908c29) x86_64 x86_64 x86_64 GNU/Linux  13:12:16  up   3:22,  5 users,  load average: 0.21, 0.26, 0.26

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -100,7 +100,7 @@ static int decode_exec(Decode *s) {
   /* wuyc */
   /* wuyc */
   /* B type */
-  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, if (src1 == src2) s->dnpc = s->pc + imm);
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, if (src1 == src2) s->dnpc = s->pc + imm; printf("%d\n", imm));
   /* wuyc */
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
