8c854686bd59791077964261564b35a8e1b6b873
tracer-ics2022 <tracer@njuics.org>
2023-09-29 00:50:43 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.19-default #1 SMP PREEMPT_DYNAMIC Tue Aug 8 22:15:01 UTC 2023 (9908c29) x86_64 x86_64 x86_64 GNU/Linux  00:50:43  up 5 days 15:00,  5 users,  load average: 0.34, 0.15, 0.12

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -15,19 +15,26 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 int sprintf(char *out, const char *fmt, ...) {
   int out_i = 0;
-  int val_num = 0;
+  /* int va_num = 0; */
   va_list ap;
+  /* int d; */
+  char *s;
   va_start(ap,fmt);
   for(; *fmt; fmt++)
   {
     if(*fmt == '%')
     {
-      val_num++;
       switch(*(++fmt))
       {
         case 's':
+          s = va_arg(ap, char *);
+          for(; *s; s++)
+          {
+            *(out+out_i) = *s;
+            out_i++;
+          }
+          *out = '\0';
           break;
-
         case 'd':
           break;
         default:
@@ -38,6 +45,7 @@ int sprintf(char *out, const char *fmt, ...) {
     else
       out[out_i++] = *fmt;
   }
+  va_end(ap);
   return out_i;
   /* panic("Not implemented"); */
 }
