e34678e0c1fd2caa4ddc6724edd12f529ce60d9f
tracer-ics2022 <tracer@njuics.org>
2023-10-01 20:07:41 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.19-default #1 SMP PREEMPT_DYNAMIC Tue Aug 8 22:15:01 UTC 2023 (9908c29) x86_64 x86_64 x86_64 GNU/Linux  20:07:41  up 8 days 10:17,  5 users,  load average: 0.09, 0.14, 0.14

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -24,13 +24,6 @@ static uint8_t *pmem = NULL;
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
-/* wuyc */
-#define Log_yellow(format, ...) \
-    _Log(ANSI_FMT(format, ANSI_FG_YELLOW) "\n", ## __VA_ARGS__)
-#define Log_cyan(format, ...) \
-    _Log(ANSI_FMT(format, ANSI_FG_CYAN) "\n", ## __VA_ARGS__)
-/* wuyc */
-
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
@@ -64,28 +57,14 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) //return pmem_read(addr, len);
-  /* wuyc */
-  {
-    word_t ret = pmem_read(addr, len);
-    Log_yellow("read " FMT_WORD " from " FMT_PADDR, ret, addr);
-    return ret;
-  }
-  /* wuyc */
+  if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) //pmem_write(addr, len, data); 
-  //wuyc
-  {
-    Log_cyan("write " FMT_WORD " to " FMT_PADDR, data, addr);
-    pmem_write(addr, len, data); 
-    return;
-  }
-  //wuyc
+  if (likely(in_pmem(addr))) pmem_write(addr, len, data); 
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -16,14 +16,29 @@
 #include <isa.h>
 #include <memory/paddr.h>
 
+/* wuyc */
+#define Log_yellow(format, ...) \
+    _Log(ANSI_FMT(format, ANSI_FG_YELLOW) "\n", ## __VA_ARGS__)
+#define Log_cyan(format, ...) \
+    _Log(ANSI_FMT(format, ANSI_FG_CYAN) "\n", ## __VA_ARGS__)
+/* wuyc */
+
 word_t vaddr_ifetch(vaddr_t addr, int len) {
   return paddr_read(addr, len);
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  /* wuyc */
+  /* return paddr_read(addr, len); */
+  word_t ret = paddr_read(addr, len);
+  Log_yellow("read " FMT_WORD " from " FMT_PADDR, ret, addr);
+  return ret;
+  /* wuyc */
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
+  /* wuyc */
+  Log_cyan("write " FMT_WORD " to " FMT_PADDR, data, addr);
+  /* wuyc */
   paddr_write(addr, len, data);
 }
