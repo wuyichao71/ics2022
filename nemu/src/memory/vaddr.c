8c1cb0b9736461294ef576190e822d119f9b905e
tracer-ics2022 <tracer@njuics.org>
2023-10-01 21:02:09 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.19-default #1 SMP PREEMPT_DYNAMIC Tue Aug 8 22:15:01 UTC 2023 (9908c29) x86_64 x86_64 x86_64 GNU/Linux  21:02:09  up 8 days 11:12,  5 users,  load average: 0.48, 0.64, 0.51

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -17,10 +17,16 @@
 #include <memory/paddr.h>
 
 /* wuyc */
+#ifdef CONFIG_MTRACE
 #define Log_yellow(format, ...) \
     _Log(ANSI_FMT(format, ANSI_FG_YELLOW) "\n", ## __VA_ARGS__)
 #define Log_cyan(format, ...) \
     _Log(ANSI_FMT(format, ANSI_FG_CYAN) "\n", ## __VA_ARGS__)
+#endif
+
+static inline bool in_mtrace(vaddr_t addr) {
+  return addr >= CONFIG_MTRACE_BEGIN && addr < CONFIG_MTRACE_END;
+}
 /* wuyc */
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
@@ -31,14 +37,20 @@ word_t vaddr_read(vaddr_t addr, int len) {
   /* wuyc */
   /* return paddr_read(addr, len); */
   word_t ret = paddr_read(addr, len);
-  Log_yellow("read " FMT_WORD " from " FMT_PADDR, ret, addr);
+#ifdef CONFIG_MTRACE
+  if(in_mtrace(addr))
+    Log_yellow("read " FMT_WORD " from " FMT_PADDR, ret, addr);
+#endif
   return ret;
   /* wuyc */
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
   /* wuyc */
-  Log_cyan("write " FMT_WORD " to " FMT_PADDR, data, addr);
+#ifdef CONFIG_MTRACE
+  if(in_mtrace(addr))
+    Log_cyan("write " FMT_WORD " to " FMT_PADDR, data, addr);
+#endif
   /* wuyc */
   paddr_write(addr, len, data);
 }
