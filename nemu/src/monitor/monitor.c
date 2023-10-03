d29d1af1b66d9322340eefebde3a5003293927c0
tracer-ics2022 <tracer@njuics.org>
2023-10-03 19:05:20 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.19-default #1 SMP PREEMPT_DYNAMIC Tue Aug 8 22:15:01 UTC 2023 (9908c29) x86_64 x86_64 x86_64 GNU/Linux  19:05:20  up 10 days  9:15,  5 users,  load average: 0.00, 0.06, 0.05

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -45,6 +45,9 @@ static void welcome() {
 void sdb_set_batch_mode();
 
 static char *log_file = NULL;
+/* wuyc */
+static char *elf_file = NULL;
+/* wuyc */
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
 static int difftest_port = 1234;
@@ -84,12 +87,18 @@ static int parse_args(int argc, char *argv[]) {
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+  /* wuyc */
+  /* while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) { */
+  /* wuyc */
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
+      /* wuyc */
+      case 'e': elf_file = optarg; break;
+      /* wuyc */
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
@@ -97,6 +106,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-e,--elf=FILE           input elf FILE\n");
         printf("\n");
         exit(0);
     }
