5713f536b48c284ee96d5940bcdd735b48d13332
tracer-ics2022 <tracer@njuics.org>
2023-10-09 00:04:12 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.19-default #1 SMP PREEMPT_DYNAMIC Tue Aug 8 22:15:01 UTC 2023 (9908c29) x86_64 x86_64 x86_64 GNU/Linux  00:04:12  up 15 days 14:14,  5 users,  load average: 0.46, 0.38, 0.28

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -18,7 +18,13 @@ char *strcpy(char *dst, const char *src) {
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i;
+  for (i = 0; i < n && src[i] != '\0'; i++)
+    dst[i] = src[i];
+  for (; i < n; i++)
+    dst[i] = '\0';
+  return dst;
+  /* panic("Not implemented"); */
 }
 
 char *strcat(char *dst, const char *src) {
@@ -30,18 +36,21 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  int dif;
-  for(size_t i = 0; (dif = s1[i] - s2[i]) == 0 && s1[i] != '\0' && s2[i] != '\0'; i++);
-  return dif;
+  size_t i = 0;
+  for(; s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0'; i++);
+  return s1[i] - s2[i];
   /* panic("Not implemented"); */
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t i = 0;
+  for(; s1[i] == s2[i] && i < n && s1[i] != '\0' && s2[i] != '\0'; i++);
+  return s1[i] - s2[i];
+  /* panic("Not implemented"); */
 }
 
 void *memset(void *s, int c, size_t n) {
-  for(size_t i = 0; i < n; i++) *(unsigned char *)(s+i) = c;
+  for(size_t i = 0; i < n; i++) *(uint8_t *)(s+i) = c;
   return s;
   /* panic("Not implemented"); */
 }
@@ -52,20 +61,16 @@ void *memmove(void *dst, const void *src, size_t n) {
 
 void *memcpy(void *out, const void *in, size_t n) {
   for(size_t i = 0; i < n; i++)
-    *(unsigned char *)(out + i) = *(unsigned char *)(in + i);
+    *(uint8_t *)(out + i) = *(uint8_t *)(in + i);
   return out;
   /* panic("Not implemented"); */
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  int dif = 0;
-  for(size_t i = 0; i < n; i++)
-  {
-    dif = *(unsigned char *)(s1+i) - *(unsigned char *)(s2+i);
-    if (dif != 0)
-      return dif;
-  }
-  return dif;
+  /* int dif = 0; */
+  size_t i = 0;
+  for(; i < n && *(uint8_t *)(s1+i) == *(uint8_t *)(s2+i); i++);
+  return *(uint8_t *)(s1+i) - *(uint8_t *)(s2+i);
   /* panic("Not implemented"); */
 }
 
