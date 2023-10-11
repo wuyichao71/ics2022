1969c6a7650736666bb5f5dcf1e38b402ce9913a
tracer-ics2022 <tracer@njuics.org>
2023-10-11 21:27:06 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.28-default #1 SMP PREEMPT_DYNAMIC Fri Sep 22 10:04:29 UTC 2023 (c11336f) x86_64 x86_64 x86_64 GNU/Linux  21:27:06  up   1:56,  5 users,  load average: 0.35, 0.41, 0.27

--- nemu/include/isa.h
+++ nemu/include/isa.h
@@ -32,15 +32,15 @@ extern char isa_logo[];
 void init_isa();
 /* wuyc */
 #ifdef CONFIG_FTRACE
-char *strtab;
+extern char *strtab;
 typedef struct
 {
   word_t st_name;
   word_t st_value;
   word_t st_size;
 } Func_Hdr;
-Func_Hdr *func_hdr;
-word_t func_num;
+extern Func_Hdr *func_hdr;
+extern word_t func_num;
 #endif
 /* wuyc */
 
