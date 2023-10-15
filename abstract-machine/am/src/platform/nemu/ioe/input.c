87d7ab27cf062b2ff703a719627ebe1374f8f544
tracer-ics2022 <tracer@njuics.org>
2023-10-15 19:21:06 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.28-default #1 SMP PREEMPT_DYNAMIC Fri Sep 22 10:04:29 UTC 2023 (c11336f) x86_64 x86_64 x86_64 GNU/Linux  19:21:06  up  21:03,  5 users,  load average: 1.24, 1.10, 0.97

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,6 +5,6 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t data = inl(KBD_ADDR);
-  kbd->keydown = data >> 31;
+  kbd->keydown = (data & KEYDOWN_MASK) >> 31;
   kbd->keycode = data & ~KEYDOWN_MASK;
 }
