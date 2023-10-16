52e152662ac2df7cd30df764002811927f7542e7
tracer-ics2022 <tracer@njuics.org>
2023-10-16 21:56:16 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.28-default #1 SMP PREEMPT_DYNAMIC Fri Sep 22 10:04:29 UTC 2023 (c11336f) x86_64 x86_64 x86_64 GNU/Linux  21:56:16  up 1 day 23:38,  5 users,  load average: 0.61, 0.47, 0.49

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -14,7 +14,7 @@ void __am_audio_init() {
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
   /* cfg->present = false; */
   cfg->present = true;
-  /* cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR); */
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
