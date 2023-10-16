98e0539c1abf9fe4b857121b481033293d895869
tracer-ics2022 <tracer@njuics.org>
2023-10-16 21:55:43 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.28-default #1 SMP PREEMPT_DYNAMIC Fri Sep 22 10:04:29 UTC 2023 (c11336f) x86_64 x86_64 x86_64 GNU/Linux  21:55:43  up 1 day 23:37,  5 users,  load average: 0.66, 0.47, 0.49

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -14,28 +14,29 @@ void __am_audio_init() {
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
   /* cfg->present = false; */
   cfg->present = true;
-  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
+  /* cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR); */
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
-  outl(AUDIO_FREQ_ADDR, ctrl->freq);
-  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
-  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
-  outl(AUDIO_INIT_ADDR, 1);
+  /* outl(AUDIO_FREQ_ADDR, ctrl->freq); */
+  /* outl(AUDIO_CHANNELS_ADDR, ctrl->channels); */
+  /* outl(AUDIO_SAMPLES_ADDR, ctrl->samples); */
+  /* outl(AUDIO_INIT_ADDR, 1); */
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = inl(AUDIO_COUNT_ADDR);
+  /* stat->count = inl(AUDIO_COUNT_ADDR); */
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-  int bufsize = io_read(AM_AUDIO_CONFIG).bufsize;
-  volatile int count = io_read(AM_AUDIO_STATUS).count;
-  while ((int)ctl->buf.end - (int)ctl->buf.start > bufsize - count)
-    count = io_read(AM_AUDIO_STATUS).count;
-  for(char *p = ctl->buf.start; p < (char *)ctl->buf.end; p++)
-  {
-    outb(AUDIO_SBUF_ADDR + count, *p);
-    count++;
-  }
+  /* int bufsize = io_read(AM_AUDIO_CONFIG).bufsize; */
+  /* volatile int count = io_read(AM_AUDIO_STATUS).count; */
+  /* int len = ctl->buf.end - ctl->buf.start; */
+  /* while (len > bufsize - count) */
+  /*   count = io_read(AM_AUDIO_STATUS).count; */
+  /* for(char *p = ctl->buf.start; p < (char *)ctl->buf.end; p++) */
+  /* { */
+  /*   outb(AUDIO_SBUF_ADDR + count, *p); */
+  /*   count++; */
+  /* } */
 }
