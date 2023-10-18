8c99e9f67ad782e181ec78b8de60f672d33cef3c
tracer-ics2022 <tracer@njuics.org>
2023-10-18 22:32:14 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.28-default #1 SMP PREEMPT_DYNAMIC Fri Sep 22 10:04:29 UTC 2023 (c11336f) x86_64 x86_64 x86_64 GNU/Linux  22:32:14  up 4 days  0:14,  5 users,  load average: 0.80, 0.33, 0.26

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -48,14 +48,14 @@ char iringbuf[IRINGBUF_LEN][128];
     log_write(ANSI_FMT(format, ANSI_FG_BLUE) "\n", ## __VA_ARGS__)
 #endif
 
-#ifdef CONFIG_WATCHPOINT
 void difftest_watchpoint(vaddr_t pc)
 {
-  /* bool changed = check_watchpoint(pc); */
-  /* if (changed) */
-    /* nemu_state.state = NEMU_STOP; */
-}
+#ifdef CONFIG_WATCHPOINT
+  bool changed = check_watchpoint(pc);
+  if (changed)
+    nemu_state.state = NEMU_STOP;
 #endif
+}
 /* wuyc */
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
