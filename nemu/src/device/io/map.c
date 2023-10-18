cc7d9b714131e6eff72053d2ce5c93dacae2f40c
tracer-ics2022 <tracer@njuics.org>
2023-10-18 21:52:06 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.28-default #1 SMP PREEMPT_DYNAMIC Fri Sep 22 10:04:29 UTC 2023 (c11336f) x86_64 x86_64 x86_64 GNU/Linux  21:52:06  up 3 days 23:34,  5 users,  load average: 0.15, 0.25, 0.22

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -76,7 +76,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   word_t ret = host_read(map->space + offset, len);
   /* wuyc */
 #ifdef CONFIG_DTRACE
-  /* if(in_dtrace(addr)) */
+  if(in_dtrace(addr))
     Log_bg_yellow("%s: read " FMT_WORD " from " FMT_PADDR, map->name, ret, addr);
 #endif
   /* wuyc */
@@ -89,7 +89,7 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   /* wuyc */
 #ifdef CONFIG_DTRACE
-  /* if(in_dtrace(addr)) */
+  if(in_dtrace(addr))
     Log_bg_cyan("%s: write " FMT_WORD " to " FMT_PADDR, map->name, data, addr);
 #endif
   /* wuyc */
