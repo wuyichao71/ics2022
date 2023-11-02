2cbecbdf81738cc657d0057113741ba6d6554367
tracer-ics2022 <tracer@njuics.org>
2023-11-02 22:13:40 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.28-default #1 SMP PREEMPT_DYNAMIC Fri Sep 22 10:04:29 UTC 2023 (c11336f) x86_64 x86_64 x86_64 GNU/Linux  22:13:40  up   0:21,  5 users,  load average: 0.24, 0.41, 0.61

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -152,7 +152,7 @@ static void write_function(Decode *s)
 
 #endif
 
-#define WRITE_CSR(scr) {t = csr; csr = src1;} while (0)
+#define WRITE_CSR(scr) do {t = csr; csr = src1;} while (0)
 
 static word_t write_csr(word_t src1, word_t csr)
 {
