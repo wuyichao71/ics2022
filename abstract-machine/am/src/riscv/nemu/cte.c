bf5a3a100c8c4b0fde3a8264f373578dfe76ca39
tracer-ics2022 <tracer@njuics.org>
2023-11-04 19:06:39 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.28-default #1 SMP PREEMPT_DYNAMIC Fri Sep 22 10:04:29 UTC 2023 (c11336f) x86_64 x86_64 x86_64 GNU/Linux  19:06:39  up 1 day 19:43,  5 users,  load average: 0.29, 0.29, 0.29

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -6,7 +6,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
   /* wuyc */
-  /*/1* */
+  /*
   for (int i = 0; i < 32; i++)
   {
     printf(" 0x%08x", c->gpr[i]);
@@ -16,10 +16,10 @@ Context* __am_irq_handle(Context *c) {
   printf("%4s: 0x%08x\n", "mcause", c->mcause);
   printf("%4s: 0x%08x\n", "mstatus", c->mstatus);
   printf("%4s: 0x%08x\n", "mepc", c->mepc);
-  /* *1/ */
+  */
+  /* printf("0x%08x\n", c); */
+  /* printf("0x%08x\n", &(c->gpr[1])); */
   /* wuyc */
-  printf("0x%08x\n", c);
-  printf("0x%08x\n", &(c->gpr[1]));
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
