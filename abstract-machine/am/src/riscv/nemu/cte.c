a70efe2996e1277ecd1c76381906d92de3c53745
tracer-ics2022 <tracer@njuics.org>
2023-11-04 19:01:46 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.28-default #1 SMP PREEMPT_DYNAMIC Fri Sep 22 10:04:29 UTC 2023 (c11336f) x86_64 x86_64 x86_64 GNU/Linux  19:01:46  up 1 day 19:38,  5 users,  load average: 0.09, 0.31, 0.31

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -19,6 +19,7 @@ Context* __am_irq_handle(Context *c) {
   /* *1/ */
   /* wuyc */
   printf("0x%08x\n", c);
+  printf("0x%08x\n", &(c->gpr[0]));
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
