af3c1369102540ea20e7ab961437be399ec6a997
tracer-ics2022 <tracer@njuics.org>
2023-11-16 21:12:35 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 5.14.21-150500.55.28-default #1 SMP PREEMPT_DYNAMIC Fri Sep 22 10:04:29 UTC 2023 (c11336f) x86_64 x86_64 x86_64 GNU/Linux  21:12:34  up 13 days 21:49,  5 users,  load average: 0.22, 0.24, 0.18

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,8 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case EVENT_YIELD: printf("event is %d\n", e.event); break;
+
     default: panic("Unhandled event ID = %d", e.event);
   }
 
