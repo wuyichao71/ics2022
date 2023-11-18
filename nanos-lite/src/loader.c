c200e6a467c6817ddb3d8e551c067381e5637f52
tracer-ics2022 <tracer@njuics.org>
2023-11-18 11:35:39 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 6.4.0-150600.1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 11 21:22:08 UTC 2023 (2051c07) x86_64 x86_64 x86_64 GNU/Linux  11:35:39  up  12:22,  5 users,  load average: 0.05, 0.16, 0.16

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -30,7 +30,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   {
     if (phdr[i].p_type == PT_LOAD)
     {
-      /* ramdisk_read() */
+      ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
+      memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
       printf("0x%x\n", phdr[i].p_filesz);
     }
   }
