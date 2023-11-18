3546e5fe2fa3ebc0c7903d2495deeacda172ae91
tracer-ics2022 <tracer@njuics.org>
2023-11-19 01:51:56 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 6.4.0-150600.1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 11 21:22:08 UTC 2023 (2051c07) x86_64 x86_64 x86_64 GNU/Linux  01:51:56  up   0:40,  5 users,  load average: 0.14, 0.25, 0.35

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,22 @@
 #include <common.h>
 #include "syscall.h"
+void sys_exit(int code);
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    /* case SYS_exit: ;break; */
+    /* wuyc */
+    case SYS_exit: sys_exit(c->GPR2);break;
     case SYS_yield: yield(); c->GPRx = 0; break;
+    /* wuyc */
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
+
+/* wuyc */
+void sys_exit(int code)
+{
+  halt(code);
+}
+/* wuyc */
