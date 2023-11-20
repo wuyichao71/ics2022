448e4bf05c6fe6c83e948491e3b39f6f376a4747
tracer-ics2022 <tracer@njuics.org>
2023-11-20 23:51:15 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 6.4.0-150600.1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 11 21:22:08 UTC 2023 (2051c07) x86_64 x86_64 x86_64 GNU/Linux  23:51:15  up 1 day  1:58,  5 users,  load average: 0.11, 0.20, 0.21

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,6 +5,7 @@
 #define SYS_format(format) format "         = %d"
 #define STRACE(SYS_type, format, ...) printf(#SYS_type format "\n", ## __VA_ARGS__)
 void sys_exit(int code);
+void sys_write(int fd, const void *buf, size_t count);
 /* wuyc */
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -18,7 +19,7 @@ void do_syscall(Context *c) {
     case SYS_exit: STRACE(sys_exit, "(%d)", c->GPR2); sys_exit(c->GPR2); break;
     /* case SYS_exit: c->GPRx=0; break; */
     case SYS_yield: yield(); c->GPRx = 0; STRACE(sys_yield, SYS_format("()"), c->GPRx); break;
-    case SYS_write: printf("hhh\n"); break;
+    case SYS_write: sys_write(c->GPR2, (const void *)c->GPR3, c->GPR4); break;
     /* wuyc */
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
@@ -29,5 +30,16 @@ void do_syscall(Context *c) {
 void sys_exit(int code)
 {
   halt(code);
+}
+
+void sys_write(int fd, const void *buf, size_t count)
+{
+  if (fd == 1 || fd == 2)
+  {
+    for (int i = 0; i < count; i++)
+    {
+      putch(((char *)buf)[i]);
+    }
+  }
 }
 /* wuyc */
