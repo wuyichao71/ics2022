893416be970beefb939be02ff7e04ef1db24522f
tracer-ics2022 <tracer@njuics.org>
2023-11-22 01:06:02 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 6.4.0-150600.1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 11 21:22:08 UTC 2023 (2051c07) x86_64 x86_64 x86_64 GNU/Linux  01:06:02  up 2 days  3:13,  5 users,  load average: 0.22, 0.20, 0.18

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,8 +2,15 @@
 #include "syscall.h"
 
 /* wuyc */
-#define SYS_format(format) format "         = %d"
-#define STRACE(SYS_type, format, ...) printf(#SYS_type format "\n", ## __VA_ARGS__)
+#define __STRACE__
+#ifdef __STRACE__
+# define SYS_format(format) format "         = %d"
+# define STRACE(SYS_type, format, ...) printf(#SYS_type format "\n", ## __VA_ARGS__)
+#else
+# define SYS_format(format) 
+# define STRACE(SYS_type, format, ...) 
+#endif
+
 void sys_exit(int code);
 void sys_write(int fd, const void *buf, size_t count);
 /* wuyc */
@@ -19,7 +26,8 @@ void do_syscall(Context *c) {
     case SYS_exit: STRACE(sys_exit, "(%d)", c->GPR2); sys_exit(c->GPR2); break;
     /* case SYS_exit: c->GPRx=0; break; */
     case SYS_yield: yield(); c->GPRx = 0; STRACE(sys_yield, SYS_format("()"), c->GPRx); break;
-    case SYS_write: sys_write(c->GPR2, (const void *)c->GPR3, c->GPR4); c->GPRx = c->GPR4; break;
+    case SYS_write: sys_write(c->GPR2, (const void *)c->GPR3, c->GPR4); 
+                    c->GPRx = c->GPR4; STRACE(sys_write, "(%d, %d, %d)", a[1], c->GPR3, c->GPR4, c->GPRx); break;
     /* wuyc */
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
