eb60a89b82583951e5c4f65b611e20ee3f23ec3f
tracer-ics2022 <tracer@njuics.org>
2023-11-24 00:59:42 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 6.4.0-150600.1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 11 21:22:08 UTC 2023 (2051c07) x86_64 x86_64 x86_64 GNU/Linux  00:59:42  up 4 days  3:07,  5 users,  load average: 0.24, 0.26, 0.31

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -125,6 +125,7 @@ static void write_function(Decode *s)
     level--;
     write_header(s->pc);
     fi = find_func(s->pc, &fn);
+    assert(fi != -1);
 
     log_write("ret  [%s]\n", fn.strtab + fi);
   }
@@ -132,6 +133,8 @@ static void write_function(Decode *s)
   {
     write_header(s->pc);
     fi = find_func(s->dnpc, &fn);
+    assert(fi != -1);
+
     log_write("call [%s@" FMT_WORD "]\n", fn.strtab + fi, s->dnpc);
     level++;
   }
