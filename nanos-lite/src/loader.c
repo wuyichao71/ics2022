514096eedc580a47270fee89dbb38edc1e62baf9
tracer-ics2022 <tracer@njuics.org>
2023-11-26 01:01:35 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 6.4.0-150600.1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 11 21:22:08 UTC 2023 (2051c07) x86_64 x86_64 x86_64 GNU/Linux  01:01:35  up 6 days  3:09,  5 users,  load average: 0.23, 0.21, 0.19

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -25,7 +25,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   assert(*(uint32_t *)ehdr.e_ident == *(uint32_t *)"\x7F""ELF");
   assert(ehdr.e_machine == EXCEPT_TYPE);
   printf("0x%x\n", *(uint32_t *)ehdr.e_ident);
-  printf("0x%08x\n", *(uint32_t *)ehdr.e_entry);
+  printf("0x%08x\n", ehdr.e_entry);
   panic("Stop");
   Elf_Phdr phdr[ehdr.e_phnum];
   /* Elf_Phdr *phdr = (Elf_Phdr *)(&ramdisk_start + ehdr.e_phoff); */
