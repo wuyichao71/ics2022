cef1691d80b768c05916a99fb03b8d1294cd6f9a
tracer-ics2022 <tracer@njuics.org>
2023-11-26 22:08:28 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux localhost.localdomain 6.4.0-150600.1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 11 21:22:08 UTC 2023 (2051c07) x86_64 x86_64 x86_64 GNU/Linux  22:08:28  up 7 days  0:16,  5 users,  load average: 0.54, 0.37, 0.29

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -91,7 +91,7 @@ void *_sbrk(intptr_t increment) {
 int _read(int fd, void *buf, size_t count) {
   /* _exit(SYS_read); */
   /* return 0; */
-  return _syscall_(SYS_read, fd, buf, count);
+  return _syscall_(SYS_read, fd, (intptr_t)buf, count);
 }
 
 int _close(int fd) {
