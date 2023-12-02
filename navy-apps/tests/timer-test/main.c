21f79c5089f93c1a3b407859a50659edfeb22d00
tracer-ics2022 <tracer@njuics.org>
2023-12-02 23:03:50 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:03:50  up   2:23,  1 user,  load average: 0.16, 0.10, 0.09

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -3,7 +3,19 @@
 
 int main()
 {
+  float sec = 0.5;
+  float us;
   struct timeval tv;
-  gettimeofday(&tv, NULL);
-  printf("s = %d, us = %d\n", tv.tv_sec, tv.tv_usec);
+  
+  while (1)
+  {
+    do
+    {
+      struct timeval tv;
+      gettimeofday(&tv, NULL);
+      us = tv.tv_sec + tv.tv_usec / 1000000;
+    } while (us < sec);
+    printf("Hello World at %d second %d microsecond\n", tv.tv_sec, tv.tv_usec);
+    sec += 0.5;
+  }
 }
