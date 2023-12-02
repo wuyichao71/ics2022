1ccf0b96067e3ca38ebbda5a86eaa47de7cdba6b
tracer-ics2022 <tracer@njuics.org>
2023-12-02 22:11:09 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:11:09  up   1:31,  1 user,  load average: 0.14, 0.11, 0.06

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -22,9 +22,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   /* int phsize; */
   fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
   /* assert(*(uint32_t *)ehdr.e_ident == 0xBadC0de); */
+  printf("0x%x\n", *(uint32_t *)ehdr.e_ident);
   assert(*(uint32_t *)ehdr.e_ident == *(uint32_t *)"\x7F""ELF");
   assert(ehdr.e_machine == EXCEPT_TYPE);
-  /* printf("0x%x\n", *(uint32_t *)ehdr.e_ident); */
   /* printf("0x%08x\n", ehdr.e_entry); */
   /* panic("Stop"); */
   Elf_Phdr phdr[ehdr.e_phnum];
