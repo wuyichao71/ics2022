00a4b01e8a1ea6ec1d6cb475683de41ca10fd3df
tracer-ics2022 <tracer@njuics.org>
2023-12-12 10:11:35 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:11:35  up   0:48,  1 user,  load average: 0.10, 0.15, 0.09

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -3,9 +3,10 @@
 
 int main() {
   NDL_Init(0);
+  printf("Hello\n");
   while (1) {
     char buf[64];
-    printf("Hello\n");
+    printf("%d\n", NDL_PollEvent(buf, sizeof(buf)));
     if (NDL_PollEvent(buf, sizeof(buf))) {
       printf("receive event: %s\n", buf);
     }
