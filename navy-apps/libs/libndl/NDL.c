0be5060c1159bcfaf59c887d8a1571b1482c3cd6
tracer-ics2022 <tracer@njuics.org>
2023-12-12 11:50:34 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:50:34  up   2:27,  1 user,  load average: 0.26, 0.18, 0.11

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -29,12 +29,12 @@ uint32_t NDL_GetTicks() {
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  FILE *f_events = fopen("/dev/events", "r");
-  int outlen = fread(buf, len, 1, f_events);
-  fclose(f_events);
-  /* int fd = open("/dev/events", 0); */
-  /* int outlen = read(fd, buf, len); */
-  /* close(fd); */
+  /* FILE *f_events = fopen("/dev/events", "r"); */
+  /* int outlen = fread(buf, len, 1, f_events); */
+  /* fclose(f_events); */
+  int fd = open("/dev/events", 0);
+  int outlen = read(fd, buf, len);
+  close(fd);
   return outlen ? 1 : 0;
 }
 /* wuyc */
