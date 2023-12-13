45bb93951d42c21f3b6d7719a0486e61bd27cae1
tracer-ics2022 <tracer@njuics.org>
2023-12-14 01:08:11 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:08:11  up   0:31,  1 user,  load average: 0.27, 0.22, 0.14

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -37,7 +37,7 @@ static Finfo file_table[] __attribute__((used)) = {
   /* wuyc */
 #include "files.h"
   {"/dev/events", 0, 0, events_read, invalid_write},
-  {"/dev/dispinfo", 0, 0, dispinfo_read, invalid_write},
+  {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
 };
 
 /* wuyc */
