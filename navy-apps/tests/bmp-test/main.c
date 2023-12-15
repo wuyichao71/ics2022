0ee7ac5359ac6444569ddc5d784e088126b7b4c8
tracer-ics2022 <tracer@njuics.org>
2023-12-16 00:43:20 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:43:20  up   0:26,  1 user,  load average: 0.33, 0.24, 0.19

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -19,7 +19,7 @@ int main() {
   /* wuyc */
   printf("WIDTH: %d\nHEIGHT: %d\n", w, h);
   int fd = open("/dev/fb", 0, 0);
-#define len 400
+#define len 5000
   uint32_t buf[len];
   for (int i = 0; i < len; i++)
     buf[i] = 0x000000FF;
