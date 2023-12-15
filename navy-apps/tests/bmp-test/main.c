5cfbd545916cfe56ef2b7481b2efb613814616dd
tracer-ics2022 <tracer@njuics.org>
2023-12-16 01:16:24 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:16:24  up   1:00,  1 user,  load average: 0.19, 0.26, 0.21

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -7,19 +7,19 @@
 #include <fcntl.h>
 
 int main() {
-  NDL_Init(0);
-  int w, h;
-  void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
-  assert(bmp);
+  /* NDL_Init(0); */
+  /* int w, h; */
+  /* void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h); */
+  /* assert(bmp); */
   /* wuyc */
   /* w = 0; */
   /* h = 0; */
   /* wuyc */
-  NDL_OpenCanvas(&w, &h);
+  /* NDL_OpenCanvas(&w, &h); */
   /* wuyc */
-  printf("WIDTH: %d\nHEIGHT: %d\n", w, h);
+  /* printf("WIDTH: %d\nHEIGHT: %d\n", w, h); */
   int fd = open("/dev/fb", 0, 0);
-  lseek(fd, 400, SEEK_SET);
+  /* lseek(fd, 400, SEEK_SET); */
 #define len 40000
   uint32_t buf[len];
   for (int i = 0; i < len; i++)
@@ -27,10 +27,10 @@ int main() {
   write(fd, buf, len);
   close(fd);
   /* wuyc */
-  NDL_DrawRect(bmp, 0, 0, w, h);
-  free(bmp);
-  NDL_Quit();
-  printf("Test ends! Spinning...\n");
-  while (1);
+  /* NDL_DrawRect(bmp, 0, 0, w, h); */
+  /* free(bmp); */
+  /* NDL_Quit(); */
+  /* printf("Test ends! Spinning...\n"); */
+  /* while (1); */
   return 0;
 }
