2ebe2d747c13b486a533053635a36eb309520e3f
tracer-ics2022 <tracer@njuics.org>
2023-12-25 10:07:55 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:07:55  up   1:17,  1 user,  load average: 0.71, 0.50, 0.31

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -33,20 +33,20 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   int w = hdr.width;
   int h = hdr.height;
   uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
-  printf("In %s: %x\n", __func__, pixels[0]);
+  /* printf("In %s: %x\n", __func__, pixels[0]); */
 
   int line_off = (w * 3 + 3) & ~0x3;
   for (int i = 0; i < h; i ++) {
     fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
     int nread = fread(&pixels[w * i], 3, w, fp);
-    printf("%d\n", nread);
+    /* printf("%d\n", nread); */
     for (int j = w - 1; j >= 0; j --) {
       uint8_t b = *(((uint8_t*)&pixels[w * i]) + 3 * j);
       uint8_t g = *(((uint8_t*)&pixels[w * i]) + 3 * j + 1);
       uint8_t r = *(((uint8_t*)&pixels[w * i]) + 3 * j + 2);
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
     }
-    printf("IN %s, line %d: %d, %d\n", __func__, __LINE__, i);
+    /* printf("IN %s, line %d: %d\n", __func__, __LINE__, i); */
   }
 
   fclose(fp);
