f0beaffa12ec1af0ecd30e62da895086c8ecfa75
tracer-ics2022 <tracer@njuics.org>
2023-12-25 10:06:04 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:06:04  up   1:15,  1 user,  load average: 0.57, 0.36, 0.25

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -45,8 +45,8 @@ void* BMP_Load(const char *filename, int *width, int *height) {
       uint8_t g = *(((uint8_t*)&pixels[w * i]) + 3 * j + 1);
       uint8_t r = *(((uint8_t*)&pixels[w * i]) + 3 * j + 2);
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
-      printf("IN %s, line %d: %d, %d\n", __func__, __LINE__, i, j);
     }
+    printf("IN %s, line %d: %d, %d\n", __func__, __LINE__, i, j);
   }
 
   fclose(fp);
