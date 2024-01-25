adec06add6176e3afee5d82277e9883710caa4f7
tracer-ics2022 <tracer@njuics.org>
2024-01-25 11:34:41 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:34:41  up   1:06,  1 user,  load average: 0.68, 0.58, 0.40

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -34,9 +34,9 @@ void init_proc() {
   /* naive_uload(NULL, "/bin/nslider"); */
   /* naive_uload(NULL, "/bin/typing-game"); */
   /* naive_uload(NULL, "/bin/dhrystone"); */
-  naive_uload(NULL, "/bin/nplayer");
+  /* naive_uload(NULL, "/bin/nplayer"); */
   /* naive_uload(NULL, "/bin/cpp-test"); */
-  /* naive_uload(NULL, "/bin/pal"); */
+  naive_uload(NULL, "/bin/pal");
   /* naive_uload(NULL, "/bin/coremark"); */
   /* naive_uload(NULL, "/bin/hello"); */
   /* wuyc */
