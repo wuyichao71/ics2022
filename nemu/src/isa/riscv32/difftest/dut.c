9bd3ba8ff37f4dec035f331e4c8c8cbcd42f2fd7
tracer-ics2022 <tracer@njuics.org>
2024-02-01 00:41:14 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:41:14  up   1:44,  1 user,  load average: 0.14, 0.27, 0.26

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -71,6 +71,7 @@ void isa_difftest_attach() {
     printf("here\n");
     printf("0x%08x\n", code_to_csr(csr_code[i]));
     /* ref_r.gpr[isa_reg_str2val("a5", &success)] = code_to_csr(csr_code[i]); */
+    ref_r.gpr[15] = code_to_csr(csr_code[i]);
     inst = 0x00079073 | csr_code[i] << 20;
     printf("0x%08x\n", inst);
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_REF);
