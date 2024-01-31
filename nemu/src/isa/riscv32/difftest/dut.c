a62531917a8116fc06723addbfa16263964ded7f
tracer-ics2022 <tracer@njuics.org>
2024-02-01 00:55:58 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:55:58  up   1:59,  1 user,  load average: 0.16, 0.14, 0.17

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -46,17 +46,17 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 
 void isa_difftest_detach() {difftest_detach();}
 
-static word_t code_to_csr(word_t code)
-{
-  switch (code)
-  {
-    case MSTATUS: return cpu.mstatus;
-    case MTVEC: return cpu.mtvec;
-    case MEPC: return cpu.mepc;
-    case MCAUSE: return cpu.mcause;
-    default: return 0;
-  }
-}
+/* static word_t code_to_csr(word_t code) */
+/* { */
+/*   switch (code) */
+/*   { */
+/*     case MSTATUS: return cpu.mstatus; */
+/*     case MTVEC: return cpu.mtvec; */
+/*     case MEPC: return cpu.mepc; */
+/*     case MCAUSE: return cpu.mcause; */
+/*     default: return 0; */
+/*   } */
+/* } */
 void isa_difftest_attach() {
   difftest_attach();
   CPU_state ref_r;
@@ -71,7 +71,7 @@ void isa_difftest_attach() {
     printf("here\n");
     printf("0x%08x\n", code_to_csr(csr_code[i]));
     /* ref_r.gpr[isa_reg_str2val("a5", &success)] = code_to_csr(csr_code[i]); */
-    ref_r.gpr[15] = code_to_csr(csr_code[i]);
+    /* ref_r.gpr[15] = code_to_csr(csr_code[i]); */
     inst = 0x00079073 | csr_code[i] << 20;
     printf("0x%08x\n", inst);
     ref_difftest_memcpy(RESET_VECTOR, &inst, sizeof(word_t), DIFFTEST_TO_REF);
