2669a267d488c905fdb9cd1f45e3cbb87dd6f182
tracer-ics2022 <tracer@njuics.org>
2024-02-01 20:22:13 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:22:13  up   1:26,  1 user,  load average: 0.22, 0.16, 0.15

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -60,7 +60,7 @@ static word_t code_to_csr(word_t code)
 void isa_difftest_attach() {
   /* CPU_state ref_r = cpu; */
   /* CPU_state ref_r = {}; */
-  word_t csr_code = {MSTATUS, MTVEC, MEPC, MCAUSE};
+  word_t csr_code[] = {MSTATUS, MTVEC, MEPC, MCAUSE};
   /* bool success; */
   /* uint32_t inst[10] = {0x800017b7, 0x47878793, 0x30579073, 0x00027b7, */ 
     /* 0x80078793, 0x30079073, 0x342022f3, 0x30002373, 0x341023f3, 0x30502473}; */
