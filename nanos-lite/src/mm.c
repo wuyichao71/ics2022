529525f20bc7ef0258e2a39b5624da756b72a6c9
tracer-ics2022 <tracer@njuics.org>
2024-02-12 00:51:43 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:51:43  up   0:21,  1 user,  load average: 0.02, 0.12, 0.14

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -10,11 +10,11 @@ void* new_page(size_t nr_page) {
 }
 
 #ifdef HAS_VME
-extern char* end;
+extern char end;
 static void* pg_alloc(int n) {
   void *ptr = new_page(n / PGSIZE);
   memset(ptr, 0, n);
-  printf("end = 0x%08x\n", end);
+  printf("end = 0x%08x\n", &end);
   return ptr;
   /* return NULL; */
 }
