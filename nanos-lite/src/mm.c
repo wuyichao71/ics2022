b7250ce5bbf2b849f154766168d2d058c868e1f5
tracer-ics2022 <tracer@njuics.org>
2024-02-12 00:57:13 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:57:13  up   0:27,  1 user,  load average: 0.05, 0.07, 0.10

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -10,11 +10,9 @@ void* new_page(size_t nr_page) {
 }
 
 #ifdef HAS_VME
-extern char end;
 static void* pg_alloc(int n) {
   void *ptr = new_page(n / PGSIZE);
   memset(ptr, 0, n);
-  printf("end = 0x%08x\n", &end);
   return ptr;
   /* return NULL; */
 }
@@ -29,6 +27,10 @@ int mm_brk(uintptr_t brk) {
   return 0;
 }
 
+extern char _data;
+extern char _bss_start;
+extern char _stack_top;
+extern char _end;
 void init_mm() {
   pf = (void *)ROUNDUP(heap.start, PGSIZE);
   Log("free physical pages starting from %p", pf);
@@ -36,4 +38,10 @@ void init_mm() {
 #ifdef HAS_VME
   vme_init(pg_alloc, free_page);
 #endif
+  /* wuyc */
+  printf("_data = 0x%08x\n", &_data);
+  printf("_bss_start = 0x%08x\n", &_bss_start);
+  printf("_stack_top = 0x%08x\n", &_stack_top);
+  printf("_end = 0x%08x\n", &_end);
+  /* wuyc */
 }
