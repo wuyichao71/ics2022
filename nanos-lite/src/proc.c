946ec3098f051ebb59928c371d590bc6df7b6359
tracer-ics2022 <tracer@njuics.org>
2024-02-14 13:14:27 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:14:27  up   4:12,  1 user,  load average: 0.15, 0.07, 0.01

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -14,13 +14,13 @@ void switch_boot_pcb() {
 }
 
 void hello_fun(void *arg) {
-  int j = 1;
-  while (1) {
-    if (j % 1000 == 0)
-      Log("Hello World from Nanos-lite with arg '%s' for the %dth time!", (uintptr_t)arg, j);
-    j ++;
+  /* int j = 1; */
+  /* while (1) { */
+  /*   if (j % 1000 == 0) */
+  /*     Log("Hello World from Nanos-lite with arg '%s' for the %dth time!", (uintptr_t)arg, j); */
+  /*   j ++; */
     yield();
-  }
+  /* } */
 }
 
 void init_proc() {
