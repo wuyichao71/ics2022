47f4f79ea55f7978c8e63c615e3998eabbfbbcd7
tracer-ics2022 <tracer@njuics.org>
2024-02-14 19:42:33 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:42:33  up   0:13,  1 user,  load average: 0.66, 0.73, 0.40

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -106,8 +106,10 @@ static bool make_token(char *e) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        /* wuyc */
+        /* Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", */
+            /* i, rules[i].regex, position, substr_len, substr_len, substr_start); */
+        /* wuyc */
 
         position += substr_len;
 
