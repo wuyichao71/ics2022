12a33539b140166a39167ee84337a8daf2493473
tracer-ics2022 <tracer@njuics.org>
2024-02-15 18:25:36 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:25:36  up  10:01,  1 user,  load average: 0.82, 0.45, 0.24

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,10 +29,10 @@ void init_proc() {
   char *argv[3] = {NULL};
   char *envp[2] = {NULL};
   /* argv[0] = "/bin/nterm"; */
-  /* argv[0] = "/bin/hello"; */
+  argv[0] = "/bin/hello";
   /* argv[0] = "/bin/dummy"; */
-  argv[0] = "/bin/pal";
-  argv[1] = "--skip";
+  /* argv[0] = "/bin/pal"; */
+  /* argv[1] = "--skip"; */
   /* envp[0] = "PATH=/bin"; */
   context_uload(&pcb[0], argv[0], argv, envp);
   context_kload(&pcb[1], hello_fun, "pcb 0");
