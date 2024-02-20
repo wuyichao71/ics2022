0eb9beb8f47a4241195cc04ce89dee502338fb5b
tracer-ics2022 <tracer@njuics.org>
2024-02-20 13:40:39 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:40:39  up   4:35,  1 user,  load average: 0.13, 0.13, 0.18

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -132,8 +132,8 @@ Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
   c->mstatus = 0x0 | MSTATUS_MXR | MSTATUS_SUM | MSTATUS_MPIE;
   c->GPRSP = (uintptr_t)kstack.end;
   c->pdir = as->ptr;
-  printf("MODE_U = %d\n", MODE_U);
-  c->np = USER;
+  /* printf("MODE_U = %d\n", MODE_U); */
+  c->np = MODE_U;
   /* c->GPR2 = (uintptr_t)arg; */
   return c;
   /* return NULL; */
