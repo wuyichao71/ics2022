bf69b14d5e05c817abd8f1d6f907dfa00d2ef1ea
tracer-ics2022 <tracer@njuics.org>
2024-02-20 13:26:19 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:26:19  up   4:21,  1 user,  load average: 1.34, 0.54, 0.27

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -18,11 +18,11 @@ typedef struct {
 } task_t;
 
 /* static char *pal_argv[] = {"/bin/pal", "--skip", NULL}; */
-/* static char *hello_argv[] = {"/bin/hello", NULL}; */
+static char *hello_argv[] = {"/bin/hello", NULL};
 static char *dummy_argv[] = {"/bin/dummy", NULL};
 task_t utask_table[] = {
-  /* {.filename = "/bin/hello", .argv = hello_argv, .envp = NULL}, */
-  {.filename = "/bin/dummy", .argv = dummy_argv, .envp = NULL},
+  {.filename = "/bin/hello", .argv = hello_argv, .envp = NULL},
+  /* {.filename = "/bin/dummy", .argv = dummy_argv, .envp = NULL}, */
   {.filename = "/bin/dummy", .argv = dummy_argv, .envp = NULL},
   /* {.filename = "/bin/pal",   .argv = pal_argv,   .envp = NULL}, */
 };
