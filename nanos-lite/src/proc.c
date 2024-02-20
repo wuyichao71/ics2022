214550475d5127d485e2043c6bcf7581b5d280a5
tracer-ics2022 <tracer@njuics.org>
2024-02-20 13:27:05 +0900

    >  compile NEMU DZ1722034-Yichao Wu 吴逸超 Linux hapolaptop 5.15.123.1-microsoft-standard-WSL2 #1 SMP Mon Aug 7 19:01:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:27:05  up   4:21,  1 user,  load average: 0.85, 0.52, 0.28

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -17,14 +17,14 @@ typedef struct {
   char **envp;
 } task_t;
 
-/* static char *pal_argv[] = {"/bin/pal", "--skip", NULL}; */
-static char *hello_argv[] = {"/bin/hello", NULL};
+static char *pal_argv[] = {"/bin/pal", "--skip", NULL};
+/* static char *hello_argv[] = {"/bin/hello", NULL}; */
 static char *dummy_argv[] = {"/bin/dummy", NULL};
 task_t utask_table[] = {
-  {.filename = "/bin/hello", .argv = hello_argv, .envp = NULL},
-  /* {.filename = "/bin/dummy", .argv = dummy_argv, .envp = NULL}, */
+  /* {.filename = "/bin/hello", .argv = hello_argv, .envp = NULL}, */
   {.filename = "/bin/dummy", .argv = dummy_argv, .envp = NULL},
-  /* {.filename = "/bin/pal",   .argv = pal_argv,   .envp = NULL}, */
+  /* {.filename = "/bin/dummy", .argv = dummy_argv, .envp = NULL}, */
+  {.filename = "/bin/pal",   .argv = pal_argv,   .envp = NULL},
 };
 /* wuyc */
 
